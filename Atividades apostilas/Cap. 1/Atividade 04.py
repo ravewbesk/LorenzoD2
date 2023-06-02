5aab4cfc4d801c23c2a23b4f6fe86fe3b33da588
ravewbesk <jasongamerbrt23@gmail.com>
2023-06-02 20:55:52 -0300

    Segundo teste commit

---
+++ Atividades apostilas/Cap. 1/Atividade 04.py
@@ -0,0 +1,10 @@
+#Cumprindo a tradição do Python
+print('Olá Mundo')
+
+#Pode usar o comando format para concatenar texto
+print('Olá {}' . format(input('Qual o seu nome? ') + '!')) # Método format da classe str
+print(f'Olá {input("Qual o seu nome? ")} !') # Método f-string
+
+
+#é possível colocar dois ou mais textos separados por vírgula
+print('Seja Bem-vindo(a) a aula de ' + 'Python II')
